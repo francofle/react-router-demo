d56ddf17f99d1cc2ba00c25832253b6c55854da6
Antonio J. Franceschi <antonio.franceschi@icloud.com>
2019-09-16 05:16:54 -0400

    routing

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ const HomePage = props => {
   console.log(props);
   return (
     <div>
-      <button onClick={() => props.history.push('/topics')}>Topics </button>
+      <Link to='/topics'>Link To Topics</Link><br/>
+      <button onClick={() => props.history.push('/topics')}>browse using the history prop</button><br/>
       <h1>HOME PAGE</h1>
     </div>
   );
@@ -17,14 +18,15 @@ const TopicsList = props => {
   return (
     <div>
       <h1>TOPIC LIST PAGE</h1>
-      <Link to={`${props.match.url}/13`}>TO TOPIC 13</Link>
-      <Link to={`${props.match.url}/17`}>TO TOPIC 17</Link>
-      <Link to={`${props.match.url}/21`}>TO TOPIC 21</Link>
+      <Link to={`${props.match.url}/13`}>Dynamic Link to Topic 13</Link><br/>
+      <Link to={`${props.match.url}/17`}>Dynamic Link to Topic 17</Link><br/>
+      <Link to={`${props.match.url}/21`}>Dynamic Link to Topic 21</Link>
     </div>
   );
 };
 
 const TopicDetail = props => {
+  console.log(props);
   return (
     <div>
       <h1>TOPIC DETAIL PAGE: {props.match.params.topicId}</h1>
@@ -36,10 +38,12 @@ function App() {
   return (
     <div>
       <Route exact path='/' component={HomePage} />
+      <Route exact path='/topics' component={TopicsList} />
+      {/*<Route path='/topics/:topicId' component={TopicDetail} />*/}
       <Route exact path='/blog/asdqw/topics' component={TopicsList} />
       <Route path='/blog/asdqw/topics/:topicId' component={TopicDetail} />
-      <Route exact path='/blog/topics' component={TopicsList} />
-      <Route path='/blog/topics/:topicId' component={TopicDetail} />
+      {/*<Route exact path='/blog/topics' component={TopicsList} />*/}
+      {/*<Route path='/blog/topics/:topicId' component={TopicDetail} />*/}
     </div>
   );
 }
